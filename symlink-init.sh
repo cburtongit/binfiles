b0089c1977ea42d87ae70c83190893befe7d30c1
Ilya Reshetnikov <devxom@gmail.com>
2017-05-28 18:16:41 +0300

    Update symlink-init.sh

--- symlink-init.sh
+++ symlink-init.sh
@@ -1,2 +1,7 @@
 ln -s ~/repo/dotfiles/config/mpv ~/.config/mpv
 ln -s ~/repo/dotfiles/config/i3 ~/.config/i3
+ln -s ~/repo/dotfiles/config/dunst ~/.config/dunst
+ln -s ~/repo/dotfiles/config/polybar ~/.config/polybar
+ln -s ~/repo/dotfiles/config/compton ~/.config/compton
+
+
